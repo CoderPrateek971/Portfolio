2c6c4d4f4b12ba19098d106b014e84709decddb2
CoderPrateek971 <prateekgarg0305@gmail.com>
2026-04-09 20:41:37 +0530

    Animation added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { IoIosArrowRoundDown } from "react-icons/io";
 import { CiDesktopMouse2 } from "react-icons/ci";
 import { FiExternalLink } from "react-icons/fi";
@@ -6,34 +6,71 @@ import { FiExternalLink } from "react-icons/fi";
 
 
 const Home = () => {
+  const fullText = "MERN-Stack Web Developer";
+  const [text, setText] = useState("");
+  const skills = [
+    "C", "C++", "Python", "Java", "JavaScript",
+    "HTML5", "CSS3", "React.js", "Node.js", "Express.js",
+    "Data Structures & Algorithms", "OOPs", "REST API", "JWT",
+    "SQL", "MongoDB", "Git & GitHub", "VS Code"
+  ];
+
+  useEffect(() => {
+    let index = 0;
+
+    const interval = setInterval(() => {
+      setText(fullText.slice(0, index + 1));
+      index++;
+
+      if (index === fullText.length) {
+        clearInterval(interval);
+      }
+    }, 80);
+    return () => clearInterval(interval);
+  }, []);
+
+  const [index, setIndex] = useState(0);
+
+
   return (
     <div className='main'>
-    <div className='intro'>
+      <div className='intro'>
 
         <div className="status">
-            <span className="dot"></span>
-            <span>Open to Opportunities</span>
+          <span className="dot"></span>
+          <span>Open to Opportunities</span>
         </div>
 
         <h1>  <span className='hi'>Hi, I'm</span> <span style={{ color: "#2EC2A0" }}>Prateek Garg</span> </h1>
-        <p className='domain'>MERN-Stack Web Developer</p>
+        <p className='domain'>{text}   <span className="cursor"></span></p>
+
+
         <p className='brief'>  B.Tech CSE student at <span className='hi'>BML Munjal University, Gurgaon</span> with a <span className='hi'>9.08 CGPA</span>, <br /> specializing in the MERN stack and building scalable web applications, while actively practicing DSA.</p>
-     
+
         <div className='buttons'>
-            <a href="projects"><button className='viewWork'>View My Work <IoIosArrowRoundDown size={20} /></button></a>
-            <a href="skills" ><button className='skillsBtn'>Tech Stack</button></a>
+          <a href="projects"><button className='viewWork'>View My Work <IoIosArrowRoundDown size={20} /></button></a>
+          <a href='#skills' ><button className='skillsBtn'>Tech Stack</button></a>
+        </div>
+        <div className="skills-banner">
+          <div className="skills-track">
+            {[...skills, ...skills].map((skill, index) => (
+              <span key={index} className="skill-text">
+                {skill}
+              </span>
+            ))}
+          </div>
         </div>
 
-        
 
-       
+
+
+      </div>
+      <div className='mouse'><CiDesktopMouse2 color='white' size={30} />
+      </div>
+
+
+
     </div>
-        <div className='mouse'><CiDesktopMouse2 color='white' size={30} />
-        </div>
-
-        
-
-</div>
   )
 }
 
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Skills = () => {
     return (
-        <div className='skills'>
+        <div className='skills' id='skills'>
             <div className='Top'>
                 <div className="aboutTag" >
                     <span >Tech Stack</span>
